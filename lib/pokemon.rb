f90129f20618f542235fab52edc2aa62ac649bb2
Erica Schimmel <ericaschimmel01@gmail.com>
2020-02-21 04:04:33 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,5 +2,15 @@ class Pokemon
   attr_accessor :pk_name, :pk_type
   attr_reader :id 
   
+  def intitalize(id = nil, pk_name, pk_type)
+    @pk_name = pk_name
+    @pk_type = pk_type
+    @id = id 
+  end 
   
+  def save 
+    sql = <<-SQL
+      
+    SQL 
+  end 
 end
